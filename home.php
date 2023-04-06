cb54f1d4a648e0ba13508dae17e0a520da4e8566
Allan <owinoallan77@gmail.com>
2023-04-06 19:38:52 +0300

    added home page

---
+++ home.php
@@ -0,0 +1,32 @@
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>@AllasheCoast.web</title>
+  <link rel="icon" type='xtype/images' href="images/pexels-photo-533851.jpeg">
+  <link rel="stylesheet" href="home.css">
+  <script src="https://kit.fontawesome.com/9acafcca7d.js" crossorigin="anonymous"></script>
+</head>
+<body>
+  
+  <header>
+    <div class="active"><button><a href="#"><i class="fa fa-home"></i> Home</a>
+    </div>
+    <ul class="list-two">
+        <li><a href="#">About</a>
+        </button></li>
+        <li><a href="#">Contact</a></li>
+        <li><a href="#">Products</a></li>
+        <li><a href="#">Company</a></li>
+        <li><a href="connect.php">Register</a></li>
+        <li><a href="#">Groups</a></li>
+        <li><a href="#">Services</a></li>
+      </ul>
+  </header>      
+
+</body>
+</html>
