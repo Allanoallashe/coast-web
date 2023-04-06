0c55320b5d9d7dd2e09010837cbae50f2c0dfea8
Allan <owinoallan77@gmail.com>
2023-04-07 00:59:41 +0300

    created header

---
+++ Coast-page/mombasa.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Allashe@mombasaPage.com</title>
+  <script src="https://kit.fontawesome.com/9acafcca7d.js" crossorigin="anonymous"></script>
+    <link rel="icon" type='xtype/images' href="images/pexels-photo-533851.jpeg">
+    <link rel="stylesheet" href="mombasa.css">
+</head>
+<body>
+  
+  <header class="head">
+    <div class="nav">
+      <ul>
+        <li class="active"><a href="#"><i class="fas fa-gem"></i> Mombasa</a></li>
+        <li><a href="#">Images</a></li>
+        <li><a href="#">Hotels</a></li>
+        <li><a href="#">More</a></li>
+      </ul>
+      <button><a href="#">Book Hotel</a></button>
+    </div>
+  </header>
+
+</body>
+</html>
