63022f23ec774cb7e8b6c150ad49743dd4ef8811
Allan <owinoallan77@gmail.com>
2023-04-10 09:22:21 +0300

    nyali links

--- Coast-page/More-pages/nyali/nyali.php
+++ Coast-page/More-pages/nyali/nyali.php
@@ -9,14 +9,14 @@
   <title>Allashe@mombasaPage.com</title>
   <script src="https://kit.fontawesome.com/9acafcca7d.js" crossorigin="anonymous"></script>
     <link rel="icon" type='xtype/images' href="images/pexels-photo-533851.jpeg">
-    <link rel="stylesheet" href="./malindi.css">
+    <link rel="stylesheet" href="./nyali.css">
 </head>
 <body>
   
   <header class="head">
     <div class="nav">
       <ul>
-        <li class="active"><a href="#"><i class="fas fa-gem"></i> Malindi</a></li>
+        <li class="active"><a href="#"><i class="fas fa-gem"></i> Nyali</a></li>
         <li><a href="#">Images</a></li>
         <li><a href="#">Hotels</a></li>
         <li id="list"><a id="drop" href="#"><i class="fa fa-caret-down"></i> More</a>
@@ -28,7 +28,7 @@
             <li class="item1"><a href="../lamu/lamu.php">Lamu</a></li>
             <li class="item2"><a href="../kilifi/kilifi.php">Kilifi</a></li>
             <li class="item3"><a href="../diani/diani.php">Diani</a></li>
-            <li class="item4"><a href="../nyali/nyali.php">Nyali</a></li>
+            <li class="item4"><a href="../malindi/malindi.php">Malindi</a></li>
             <li class="item5"><a href="#">Mombasa</a></li>
             <li class="item6"><a href="#">Bamburi</a></li>
           </ul>
@@ -45,7 +45,7 @@
 
   <!-- coats -->
   <div class="main">
-        <p class="title">Malindi Images</p>
+        <p class="title">Nyali Images</p>
     <div class="img-container">
       <div class="images">
         <img id="img" src="./images/The-Coast/th.jpg" alt="Theme">
@@ -104,7 +104,7 @@
   </div>
   <!-- hotels -->
   <div class="main">
-        <p class="title1">Malindi Hotels</p>
+        <p class="title1">Nyali Hotels</p>
     <div class="img-container">
       <div class="images">
         <img src="./images/hotels/h1.jpg" alt="Theme">
@@ -190,6 +190,6 @@
       </div>
     </footer>
 
-    <script src="./malindi.js" type="text/javascript"></script>
+    <script src="./nyali.js" type="text/javascript"></script>
 </body>
 </html>
