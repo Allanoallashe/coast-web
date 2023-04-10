d6406af84f9c2bd9ecfa365892b9ab1b43472b4a
Allan <owinoallan77@gmail.com>
2023-04-10 10:12:33 +0300

    logo

--- Coast-pages/More-pages/bamburi/bamburi.php
+++ Coast-pages/More-pages/bamburi/bamburi.php
@@ -15,7 +15,7 @@
   <header class="head">
     <div class="nav">
       <ul>
-        <li class="active"><a href="#"><i class="fas fa-gem"></i> Bamburi</a></li>
+        <li class="active"><a href="#"><i class="fa-brands fa-fort-awesome"></i> Bamburi</a></li>
         <li><a href="#">Images</a></li>
         <li><a href="#">Hotels</a></li>
         <li id="list"><a id="drop" href="#"><i class="fa fa-caret-down"></i> More</a>
