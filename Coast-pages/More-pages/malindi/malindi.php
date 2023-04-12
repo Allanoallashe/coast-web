9936a8ef88c8d0c14e267d053573578cf83a3574
Allan <owinoallan77@gmail.com>
2023-04-12 11:58:54 +0300

    malindi page

--- Coast-pages/More-pages/malindi/malindi.php
+++ Coast-pages/More-pages/malindi/malindi.php
@@ -1,25 +1,32 @@
 
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Allashe@mombasaPage.com</title>
+  <title>Allashe@DianiPage.com</title>
   <script src="https://kit.fontawesome.com/9acafcca7d.js" crossorigin="anonymous"></script>
     <link rel="icon" type='xtype/images' href="./images/coast/pexels-photo-533851.jpeg">
-    <link rel="stylesheet" href="./malindi.css">
+    <link rel="stylesheet" href="./lamu.css">
 </head>
 <body>
   
   <header class="head">
     <div class="nav">
+      <ol>
+        <a href="./malindi.php">
+        <i class="fa-brands fa-fort-awesome fa-beat"></i> Malindi</a>
+     </ol>
+      <main class="active">
+        <a><i id="pope" class="fa-solid fa-bars"></i></a>
+      </main>
       <ul>
-        <li class="active"><a href="#"><i class="fa-brands fa-fort-awesome fa-beat"></i> Malindi</a></li>
+        <li><a href="./malindi.php"><i class="fa-brands fa-fort-awesome fa-beat"></i> Malindi</a></li>
         <li><a href="#">Images</a></li>
-        <li><a href="#title1">Restaurants</a></li>
-        <li id="list"><a id="drop" href="#"><i class="fa fa-caret-down"></i> More</a>
+        <li><a href="#map">Map</a></li>
+        <li><a href="#title">Restaurants</a></li>
+        <li id="list"><a id="drop"><i class="fa fa-caret-down"></i> More</a>
          <div id="more">
           <ul id="browse1">
             <div class="close">
@@ -29,8 +36,8 @@
             <li class="item2"><a href="../kilifi/kilifi.php">Kilifi</a></li>
             <li class="item3"><a href="../diani/diani.php">Diani</a></li>
             <li class="item4"><a href="../nyali/nyali.php">Nyali</a></li>
-            <li class="item5"><a href="../Mombasa/mombasa.php">Mombasa</a></li>
-            <li class="item6"><a href="../bamburi/bamburi.php">Bamburi</a></li>
+            <li class="item5"><a href="../bamburi/bamburi.php">Bamburi</a></li>
+            <li class="item6"><a href="../Mombasa/mombasa.php">Mombasa</a></li>
           </ul>
       </div>
       </li>
@@ -39,127 +46,279 @@
     </div>
   </header>
 
-  <div class='marque'>
-    <marquee width="98%"  direction="left" height="100%" behavior="scroll" scrollamount="5" loop="100000">The coastal regions i.e. the coast of Kenya, are very nice places to visit especially during the holidays. The beaches have very nice aesthetic beauty. Register to get started and book your hotels prier🥳.Accomodations are there in plenty&#128150;  &#128519; 🥳</marquee>
+    <!-- header display -->
+  <div id="display">
+        <div class="closer">
+              <i id="zip" class="fa-solid fa-xmark"></i>
+            </div>
+       <li><a href="#" class="swipe1">Images</a></li>
+       <li><a class="swipe2" href="#map">Map</a></li>
+       <li><a class="swipe3"href="#title">Restaurants</a></li>
+       <li><a id="lister"><i class="fa fa-caret-down"></i> More Locations</a></li>
   </div>
 
-  <!-- coats -->
-  <div class="main">
-        <p class="title">Malindi Images</p>
-    <div class="img-container">
-      <div class="images">
-        <img id="img" src="./images/coast/m1.jpg" alt="Theme">
+     <!-- more popper -->
+     <div class="more1" id="more1">
+          <ul id="browse-two">
+            <div class="closing">
+              <i id="ex" class="fa-solid fa-xmark"></i>
+            </div>
+            <li id="nero1"><a href="../lamu/lamu.php">Lamu</a></li>
+            <li id="nero2"><a href="../kilifi/kilifi.php">Kilifi</a></li>
+            <li id="nero3"><a href="../diani/diani.php">Diani</a></li>
+            <li id="nero4"><a href="../nyali/nyali.php">Nyali</a></li>
+            <li id="nero5"><a href="../bamburi/bamburi.php">Bamburi</a></li>
+            <li id="nero6"><a href="../Mombasa/mombasa.php">Mombasa</a></li>
+          </ul>
       </div>
-      <div class="images">
-        <img src="./images/coast/m10.jpg" alt="pwani">
-      </div>
-      <div class="images">
-        <img src="./images/coast/m11.jpg" alt="pwani">
-      </div>
-    </div>
-    <div class="img-container">
-      <div class="images">
-        <img src="./images/coast/m12.jpg" alt="Theme">
-      </div>
-      <div class="images">
-        <img src="./images/coast/m13.jpg" alt="pwani">
-      </div>
-      <div class="images">
-        <img src="./images/coast/m14.jpg" alt="pwani">
-      </div>
-    </div>
-    <div class="img-container">
-      <div class="images">
-        <img src="./images/coast/m15.jpg" alt="Theme">
-      </div>
-      <div class="images">
-        <img src="./images/coast/m16.jpg" alt="pwani">
-      </div>
-      <div class="images">
-        <img src="./images/coast/m17.jpg" alt="pwani">
-      </div>
-    </div>
-    <div class="img-container">
-      <div class="images">
-        <img src="./images/coast/m18.jpg" alt="Theme">
-      </div>
-      <div class="images">
-        <img src="./images/coast/m19.jpg" alt="pwani">
-      </div>
-      <div class="images">
-        <img src="./images/coast/m21.jpg" alt="pwani">
-      </div>
-    </div>
-    <div class="img-container">
-      <div class="images">
-        <img src="./images/coast/m4.jpg" alt="Theme">
-      </div>
-      <div class="images">
-        <img src="./images/coast/m6.jpg" alt="pwani">
-      </div>
-      <div class="images">
-        <img src="./images/coast/m8.jpg" alt="pwani">
-      </div>
-    </div>
+
+     <!-- marque -->
+  <div class='marque'>
+    <marquee width="98%"  direction="left" height="100%" behavior="scroll" scrollamount="5" loop="100000">The coastal regions i.e. the coast of Kenya, are very nice places to visit especially during the holidays. The beaches have very nice aesthetic beauty. Register to get started and book your hotels prier🥳.Accomodations are there in plenty&#128150;  &#128519;. Welcome to the Coast of Kenya🥳</marquee>
   </div>
-  <!-- hotels -->
+
+      <!-- popup image -->
+      <div class="popup-image">
+        <span>&times;</span>
+        <img src="./images/coast/pexels-photo-533851.jpeg" alt="popup">
+      </div>
+
+  <!-- coasts -->
+   <p class="title">Lamu Images</p>
   <div class="main">
-        <p id="title1" class="title1">Malindi Restaurants</p>
+
+    <div class="img-container">
+      <div class="images">
+        <img src="./images/coast/L1.jpg" alt="Theme">
+      </div>
+    </div>
+
+    <div class="img-container">
+      <div class="images">
+        <img src="./images/coast/L10.jpg" alt="Theme">
+      </div>
+    </div>
+
+    <div class="img-container">
+      <div class="images">
+        <img src="./images/coast/L11.jpg" alt="Theme">
+      </div>
+    </div>
+
+    <div class="img-container">
+      <div class="images">
+        <img src="./images/coast/L12.jpg" alt="Theme">
+      </div>
+    </div>
+
+    <div class="img-container">
+      <div class="images">
+        <img src="./images/coast/L13.jpg" alt="Theme">
+      </div>
+    </div>
+
+    <div class="img-container">
+      <div class="images">
+        <img src="./images/coast/L14.jpg" alt="Theme">
+      </div>
+    </div>
+
+    <div class="img-container">
+      <div class="images">
+        <img src="./images/coast/L15.jpg" alt="Theme">
+      </div>
+    </div>
+
+    <div class="img-container">
+      <div class="images">
+        <img src="./images/coast/L16.jpg" alt="Theme">
+      </div>
+    </div>
+    <div class="img-container">
+      <div class="images">
+        <img src="./images/coast/L17.jpg" alt="Theme">
+      </div>
+    </div>
+    <div class="img-container">
+      <div class="images">
+        <img src="./images/coast/L18.jpg" alt="Theme">
+      </div>
+    </div>
+    <div class="img-container">
+      <div class="images">
+        <img src="./images/coast/L19.jpg" alt="Theme">
+      </div>
+    </div>
+    <div class="img-container">
+      <div class="images">
+        <img src="./images/coast/L20.jpg" alt="Theme">
+      </div>
+    </div>
+    <div class="img-container">
+      <div class="images">
+        <img src="./images/coast/L2.jpg" alt="Theme">
+      </div>
+    </div>
+    <div class="img-container">
+      <div class="images">
+        <img src="./images/coast/L21.jpg" alt="Theme">
+      </div>
+    </div>
+    <div class="img-container">
+      <div class="images">
+        <img src="./images/coast/L3.jpg" alt="Theme">
+      </div>
+    </div>
+    <div class="img-container">
+      <div class="images">
+        <img src="./images/coast/L4.jpg" alt="Theme">
+      </div>
+    </div>
+    <div class="img-container">
+      <div class="images">
+        <img src="./images/coast/L5.jpg" alt="Theme">
+      </div>
+    </div>
+    <div class="img-container">
+      <div class="images">
+        <img src="./images/coast/L6.jpg" alt="Theme">
+      </div>
+    </div>
+    <div class="img-container">
+      <div class="images">
+        <img src="./images/coast/L8.jpg" alt="Theme">
+      </div>
+    </div>
+    <div id='title' class="img-container">
+      <div class="images">
+        <img src="./images/coast/L9.jpg" alt="Theme">
+      </div>
+    </div>
+
+  </div>
+
+
+  <!-- hotels -->
+     <p id="title1" class="title1">Malindi Restaurants</p>
+   <div class="main">
+    <div class="img-container">
+      <div class="images">
+        <img src="./images/hotels/h20.jpg" alt="Theme">
+      </div>
+    </div>
+
     <div class="img-container">
       <div class="images">
         <img src="./images/hotels/h1.jpg" alt="Theme">
       </div>
+    </div>
+
+    <div class="img-container">
       <div class="images">
-        <img src="./images/hotels/h10.jpg" alt="pwani">
-      </div>
-      <div class="images">
-        <img src="./images/hotels/h11.jpg" alt="pwani">
+        <img src="./images/hotels/h10.jpg" alt="Theme">
       </div>
     </div>
+
+    <div class="img-container">
+      <div class="images">
+        <img src="./images/hotels/h11.jpg" alt="Theme">
+      </div>
+    </div>
+
     <div class="img-container">
       <div class="images">
         <img src="./images/hotels/h12.jpg" alt="Theme">
       </div>
+    </div>
+
+    <div class="img-container">
       <div class="images">
-        <img src="./images/hotels/h13.jpg" alt="pwani">
-      </div>
-      <div class="images">
-        <img src="./images/hotels/h14.jpg" alt="pwani">
+        <img src="./images/hotels/h13.jpg" alt="Theme">
       </div>
     </div>
+
+    <div class="img-container">
+      <div class="images">
+        <img src="./images/hotels/h14.jpg" alt="Theme">
+      </div>
+    </div>
+
     <div class="img-container">
       <div class="images">
         <img src="./images/hotels/h15.jpg" alt="Theme">
       </div>
+    </div>
+    <div class="img-container">
       <div class="images">
-        <img src="./images/hotels/h16.jpg" alt="pwani">
+        <img src="./images/hotels/h16.jpg" alt="Theme">
       </div>
+    </div>
+    <div class="img-container">
       <div class="images">
-        <img src="./images/hotels/h17.jpg" alt="pwani">
+        <img src="./images/hotels/h17.jpg" alt="Theme">
       </div>
     </div>
     <div class="img-container">
       <div class="images">
         <img src="./images/hotels/h18.jpg" alt="Theme">
       </div>
+    </div>
+    <div class="img-container">
       <div class="images">
-        <img src="./images/hotels/h19.jpg" alt="pwani">
-      </div>
-      <div class="images">
-        <img src="./images/hotels/h20.jpg" alt="pwani">
+        <img src="./images/hotels/h19.jpg" alt="Theme">
       </div>
     </div>
     <div class="img-container">
       <div class="images">
-        <img src="./images/hotels/h21.jpg" alt="Theme">
-      </div>
-      <div class="images">
-        <img src="./images/hotels/h5.jpg" alt="pwani">
-      </div>
-      <div class="images">
-        <img src="./images/hotels/h9.jpg" alt="pwani">
+        <img src="./images/hotels/h3.jpg" alt="Theme">
       </div>
     </div>
+    <div class="img-container">
+      <div class="images">
+        <img src="./images/hotels/h4.jpg" alt="Theme">
+      </div>
+    </div>
+    <div class="img-container">
+      <div class="images">
+        <img src="./images/hotels/h6.jpg" alt="Theme">
+      </div>
+    </div>
+    <div class="img-container">
+      <div class="images">
+        <img src="./images/hotels/h2.jpg" alt="Theme">
+      </div>
+    </div>
+    <div class="img-container">
+      <div class="images">
+        <img src="./images/hotels/h5.jpg" alt="Theme">
+      </div>
+    </div>
+    <div class="img-container">
+      <div class="images">
+        <img src="./images/hotels/h9.jpg" alt="Theme">
+      </div>
+    </div>
+    <div class="img-container">
+      <div class="images">
+        <img src="./images/hotels/h8.jpg" alt="Theme">
+      </div>
+    </div>
+    <div id="map" class="img-container">
+      <div class="images">
+        <img src="./images/hotels/h7.jpg" alt="Theme">
+      </div>
+    </div>
+  </div>
+
+  <div class="map">
+    <div class="map-description">
+     <p>Malindi Map</p>
+      <span>
+        <i id="moon" class="fa-solid fa-moon"></i>
+        <i id="sun" class="fa-solid fa-sun"></i>
+      </span>
+    </div>
+   <iframe id="mapi" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d7973.384828288841!2d40.89775900419923!3d-2.2681793897741382!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x1817193e959723d9%3A0xb9894b314f651fd6!2sLamu%20Fort!5e0!3m2!1sen!2ske!4v1681287625501!5m2!1sen!2ske" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
   </div>
 
 <!-- footer -->
@@ -169,7 +328,8 @@
                   <i class="fas fa-gem">ALLANITY</i></a>
         </div>
       <p class="website__rights">
-              <span class="dot">C</span> @allashe.com 2023. All rights reserved.
+              <span class="dot"><strong>C</strong></span> 
+              <span class="text">@allashe.com 2023. All rights reserved.</span>
       </p>
       <div class="social__icons">
           <a href="/" class="social__icon-link" target="_blank">
@@ -190,6 +350,6 @@
       </div>
     </footer>
 
-    <script src="./malindi.js" type="text/javascript"></script>
+    <script src="./lamu.js" type="text/javascript"></script>
 </body>
 </html>
